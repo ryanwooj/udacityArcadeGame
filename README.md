de9a9f3805c229a7d28e5d00fd08bf8345b0f14f
Ryan <devrwoo@gmail.com>
2018-09-06 16:48:06 -0700

    Final Version

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 frontend-nanodegree-arcade-game
 ===============================
 
-Students should use this [rubric](https://review.udacity.com/#!/projects/2696458597/rubric) for self-checking their submission. Make sure the functions you write are **object-oriented** - either class functions (like Player and Enemy) or class prototype functions such as Enemy.prototype.checkCollisions, and that the keyword 'this' is used appropriately within your class and class prototype functions to refer to the object the function is called upon. Also be sure that the **readme.md** file is updated with your instructions on both how to 1. Run and 2. Play your arcade game.
+##Description:
 
-For detailed instructions on how to get started, check out this [guide](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true).
+This game is very Fun! Your goal is to cross the river without stepping on a bug!
+
+##How to Play:
+
+Very Simple!
+Use the arrow key in your keyboard to move around across the board with your character!
+
+
+##Enjoy it!
--- js/app.js
+++ js/app.js
@@ -1,36 +1,119 @@
+
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+class Enemy {
+  // Variables applied to each of our instances go here
+  constructor (x, y, movement) {
+    this.x = x;
+    this.y = y;
+    this.movement = movement;
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
-};
-
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
+  }
+  // Update the enemy's position, required method for game
+  // Parameter: dt, a time delta between ticks
+  update (dt) {
     // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
+    // which will ensure the game runs at the same movement for
     // all computers.
-};
+    this.x += this.movement * dt;
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+    //if off canvas, reset enemy position to left
+    if (this.x > 550 ) {
+      this.x = -100;
+      this.movement = 100 + Math.floor(Math.random() * 500);
+    }
+
+     // 505/5 = 101, 606/6 = 101
+      //101- 64 = 37x * 83-64 = 19 y
+    if (player.x < this.x + 70 &&
+        player.x + 37 > this.x &&
+        player.y < this.y + 70 &&
+        player.y + 37 > this.y)
+    {
+        player.x = 200;
+        player.y = 380;
+
+        //Background Warning Pink sign if player and enemy crush
+        document.querySelector('body').style.backgroundColor = 'pink';
+        setTimeout( () => {
+        document.querySelector('body').style.backgroundColor='white';
+        alert('Beep! You Just Missed it!') }
+        , 500 );
+    }
+  }
+  // Draw the enemy on the screen, required method for game
+  render() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y)
+  }
+}
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 
+class Player {
+  constructor(x, y, movement) {
+    this.x = x;
+    this.y = y;
+    this.movement = movement;
+    this.sprite = 'images/char-boy.png';
+  }
+  update() {
+    if (this.y > 380) {
+      this.y = 380
+    }
+    if (this.x > 400) {
+      this.x = 400;
+    }
+    if (this.x < 0) {
+      this.x = 0;
+    }
+    if (this.y < 0) {
+      this.x = 200;
+      this.y = 380;
+      document.querySelector('body').style.backgroundColor = 'green';
+      setTimeout(() => {
+        document.querySelector('body').style.backgroundColor = 'white';
+        alert('Congrats! You Won!');
+      }, 500);
+    }
+  }
+  render() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  }
+
+  handleInput(keyPress) {
+    switch (keyPress) {
+      case 'up':
+        this.y -= this.movement + 30;
+        break;
+      case 'down':
+        this.y += this.movement + 30;
+        break;
+      case 'right':
+        this.x += this.movement + 51;
+        break;
+      case 'left':
+        this.x -= this.movement + 51;
+        break;
+    }
+  };
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+let allEnemies = [];
+const enemyPosition = [60, 140, 220];
+const player = new Player(200, 380, 50);
+let enemy;
 
-
+enemyPosition.forEach( yPosition => {
+  enemy = new Enemy(0, yPosition, 100 + Math.floor(Math.random() * 500));
+  allEnemies.push(enemy);
+});
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
